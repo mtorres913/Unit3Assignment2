9fde7ac755105b5bbaacc28cf359962dce32ca92
MIguel Torres <mtorres913@gmail.com>
2025-08-31 00:55:47 -0500

    Add solve method with single-path traversal logic

--- CaveExplorer.java
+++ CaveExplorer.java
@@ -24,5 +24,29 @@ public class CaveExplorer {
     }
     return sb.toString();
 }
+public boolean solve() {
+    boolean[][] visited = new boolean[cave.length][cave[0].length];
+    int row = startRow;
+    int col = startCol;
+
+    while (true) {
+        visited[row][col] = true;
+        if (cave[row][col] == 'M') return true;
+
+        // Try moving in one of four directions (only one will work)
+        if (isValid(row, col - 1, visited)) col--; // West
+        else if (isValid(row, col + 1, visited)) col++; // East
+        else if (isValid(row - 1, col, visited)) row--; // North
+        else if (isValid(row + 1, col, visited)) row++; // South
+        else break; // No valid moves
+    }
+
+    return false;
+}
+
+private boolean isValid(int r, int c, boolean[][] visited) {
+    return r >= 0 && r < cave.length && c >= 0 && c < cave[0].length &&
+           !visited[r][c] && (cave[r][c] == '.' || cave[r][c] == 'M');
+}
 
 }
